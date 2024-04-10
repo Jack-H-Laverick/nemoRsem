e13ec85d6168f87e0c582370dcfc56a4aa840164
Jack Laverick <jack.laverick@strath.ac.uk>
2024-04-10 11:19:29 +0100

    feat: copy in transect utility function, and also a function to streamline classifying files by name.

--- R/Spatial utilities.R
+++ R/Spatial utilities.R
@@ -1,6 +1,45 @@
 
 #### Utilities to help with manipulating data ####
 
+
+#' Categorise files for processing with `NEMO_ERSEM`
+#'
+#' This function takes a directory, recursively lists the file names, and extracts meaningful meta-data
+#'
+#' @param dir A path string to the directory containing NEMO-ERSEM netcdf files.
+#' @param recursive T/F whether to process files in all sub-directories.
+#' @return A dataframe that can be passed to `NEMO_ERSEM` using pmap.
+#' @family NEMO-ERSEM spatial tools
+#' @export
+categorise_files <- function(dir, recursive = TRUE) {
+
+
+  ersem_files <- list.files(dir, recursive = recursive, full.names = TRUE, pattern = ".nc") %>%  # Retunr netcdf files in the directory
+    as.data.frame() %>%                                                                     # Turn the vector into a dataframe
+    tidyr::separate(".", into = c("Path", "File"), sep = "STRATH") %>%                      # Split file names from path for ease
+    dplyr::mutate(File = paste0("STRATH", File)) %>%                                        # Reintroduce separation character string
+    dplyr::mutate(date = stringr::str_sub(File, start = -9, end = -4),                      # Pull time information
+           Month = stringr::str_sub(date, start = 5, end = 6),
+           Year = stringr::str_sub(date, start = 1, end = 4)) %>%
+    dplyr::mutate(String = dplyr::case_when(stringr::str_detect(File, "thetao_con") ~ "thetao_con-Temperature", # Categorise the file types
+                            stringr::str_detect(File, "so_abs") ~ "so_abs-Salinity",
+                            stringr::str_detect(File, "uo") ~ "uo-Zonal currents",
+                            stringr::str_detect(File, "vo") ~ "vo-Meridional currents",
+                            stringr::str_detect(File, "wo") ~ "wo-Vertical velocity",
+                            stringr::str_detect(File, "difvho") ~ "difvho-Vertical diffusivity",
+                            stringr::str_detect(File, "R1") ~ "R1-Dissolved organic nitrogen",
+                            stringr::str_detect(File, "O2") ~ "O2-Oxygen",
+                            stringr::str_detect(File, "N4") ~ "N4-Ammonium",
+                            stringr::str_detect(File, "N3") ~ "N3-Nitrate",
+                            stringr::str_detect(File, "RP") ~ "RP-Detritus (Particulate organic nitrogen)",
+                            stringr::str_detect(File, "B1") ~ "B1-Bacterial nitrogen",
+                            stringr::str_detect(File, "P1") ~ "P1-Diatom nitrogen",
+                            stringr::str_detect(File, "P234_n") ~ "P234_n-Other phytoplankton nitrogen")) %>%
+    tidyr::separate("String", into = c("Type", "Name"), sep = "-") %>%                     # cheat way to succinctly get both a "type" and named variable column
+    dplyr::select(Path, File, date, Year, Month, Type, Name)                               # Limit to columns of use
+
+}
+
 #' Calculate Water Layer Thicknesses Within a Vector
 #'
 #' This function calculates the thickness of the water layer around each point in a vector. These thicknesses
@@ -73,11 +112,11 @@ calculate_proximity_weight <- function(depths, target) rev(diff(c(depths[1], tar
 #'
 #' This function gets the latitudes, longitudes, and depths which define the spatial location of points in an array of NEMO-ERSEM outputs.
 #'
-#' Each variable of interest in the netcdf file is imported, and then collected into a list. A T/F switch is included for
-#' when the file is a grid_W type as the depth levels are different and stored differently.
+#' Each variable of interest in the netcdf file is imported, and then collected into a list.
 #'
 #' @param file The full name of a netcdf file.
-#' @param grid_W Is the file a grid_W file, TRUE or FALSE. Needed as the depth variable is unique to this file type.
+#' @param depthvar The name of the depth variable netcdf file (when present).
+#' @param depthvar The name of the depth dimension in a netcdf file (when extracting values from dimension metadata).
 #' @return A list of three elements:
 #' \itemize{
 #'  \item{\emph{nc_lat -}}{ A matrix of latitudes which maps onto the first and second dimension of a NEMO-ERSEM array.}
@@ -86,17 +125,15 @@ calculate_proximity_weight <- function(depths, target) rev(diff(c(depths[1], tar
 #'  }
 #' @family NEMO-ERSEM variable extractors
 #' @export
-get_spatial <- function(file, grid_W = F) {
+get_spatial <- function(file, depthvar = NULL, depthdim = NULL) {
 
   nc_raw <- ncdf4::nc_open(file)                       # Open up a netcdf file to see it's raw contents (var names)
 
   nc_lat <- ncdf4::ncvar_get(nc_raw, "nav_lat")        # Extract a matrix of all the latitudes
   nc_lon <- ncdf4::ncvar_get(nc_raw, "nav_lon")        # Extract a matrix of all the longitudes
 
-  if(grid_W == F) {                                    # Extract a vector of depths
-    nc_depth <- nc_raw$dim$deptht$vals} else {
-    nc_depth <- nc_raw$dim$depthw$vals
-    }
+  if(is.null(depthvar) == FALSE) nc_depth <- ncdf4::ncvar_get(nc_raw, depthvar)
+  if(is.null(depthdim) == FALSE) eval(parse(text = paste0("nc_depth <- nc_raw$dim$",depthdim,"$vals")))
 
   ncdf4::nc_close(nc_raw)                              # You must close an open netcdf file when finished to avoid data loss
 
@@ -143,4 +180,152 @@ voronoi_grid <- function(points, area) {
     dplyr::bind_rows() %>%                                         # Combine the results from each area
     sf::st_sf(geomc = .$geometry, crs = 4326)                      # Reinstate attributes of the geometry column
 
+}
+
+#' Summarise NEMO-ERSEM Output into Time Series Along Transects
+#'
+#' This function averages NEMO-ERSEM monthly summaries into time series for the target boundaries of StrathE2E.
+#'
+#' The function subsets the NEMO-ERSEM grid according to the transects object provided. Water exchanges between
+#' model compartments are totaled. The boundary conditions of the model domain for variables needed by
+#' StrathE2E are summarised as a flow-weighted mean, applying the flow rate at each transect.
+#'
+#' @param saved A dataframe containing a summarised month from NEMO-ERSEM model outputs.
+#' @param transects A dataframe containing the labelled transects along the model domain boundaries.
+#' @param vars A character vector containing the column names to be summarised for boundary conditions. Defaults to the targets for StrathE2E
+#' @return A list containing two summaries.
+#' \itemize{
+#'  \item{Element 1}{A dataframe containing the total water exchanged between model compartments.}
+#'  \item{Element 2}{A dataframe containing the flow-weighted boundary conditions around the model domain.}}
+#' @family NEMO-MEDUSA averages
+#' @export
+NE_boundary_summary <- function(saved, transects, vars = c("NO3", "NH4", "Chlorophyll", "Temperature")) {
+
+  Data <- readRDS(saved) %>%                                                  # Import a NM summary object
+    dplyr::select(-c(Shore, weights))                                         # Drop duplicated columns which vonflict
+  data.table::setDT(Data, key = c("x", "y", "slab_layer"))                    # Convert to a data.table keyed spatially for quick summaries.
+
+  join <- Data[transects] %>%                                                 # 0.5% of the transects don't catch data (NA), it's because the deep offshore layer doesn't have a buffer on the shore side.
+    dplyr::mutate(Flow = ifelse(current == "Zonal", Zonal, Meridional)) %>%   # Grab current perpendicular to the transect
+    dplyr::mutate(Flow = ifelse(Flip == T, -1*Flow, Flow)) %>%                # Correct flow so that + always goes IN to a model box
+    dplyr::mutate(Flow = Flow * weights,                                      # Weight by transect area to get the volume of water
+                  Direction = ifelse(Flow > 0, "In", "Out"))                         # Label direction based on flow rate
+
+  ## Summarise water exchanges
+
+  water <- join[, .(Flow = sum(Flow, na.rm = T)),                             # Tally up water movements
+                by = c("Shore", "slab_layer", "Direction",                    # By exchanges we want to keep track of
+                       "Neighbour", "Month", "Year")] %>%
+    tidyr::drop_na()                                                          # The NA transects introduce a dead group, remove.
+
+  ## Summarise boundary conditions
+
+  #*# How do we weight by flow? some are negative
+  boundary <- join[perimeter == T & Direction == "In",                        # For transects which bound the perimeter of the model domain
+                   lapply(.SD, weighted.mean, w = Flow, na.rm = T),           # Flow-weighted mean of target variables
+                   by = c("Shore", "slab_layer", "Neighbour",                 # By groups we want to keep track of
+                          "Month", "Year"),
+                   .SDcols = vars] %>%                                        # Specify target variables
+    tidyr::drop_na() %>%                                                      # The NA transects introduce a dead group, remove.
+    dplyr::mutate(Date = as.Date(paste(15, Month, Year, sep = "/"), format = "%d/%m/%Y"),
+                  Compartment = paste(Shore, slab_layer)) %>%
+    tidyr::pivot_longer(eval(vars), names_to = "Variable", values_to = "Measured") # reshape
+
+  result <- list(Flows = water, Boundarys = boundary)                         # Combine both summaries so they can be returned together
+
+  return(result)
+}
+
+#' Summarise NEMO-ERSEM Output into Time Series Within Model Compartments
+#' This function averages NEMO-ERSEM monthly summaries into time series for each model compartment.
+#'
+#' The function groups by model compartment (Depth and Shore zone) and time step (Month and Year).
+#' The mean for every target variable is calculated within these groups.
+#'
+#' The ice-threshold parameter controls how high the concentration of ice must be at a pixel to count as ice-affected.
+#' The default is set to 0, so any pixels with ice are counted, but if you decide that ice-concentrations below 0.05
+#' are meaningless for your purposes, you can set the threshold to have these pixels classed as ice free instead. This will
+#' also drop the pixels from calculations of Ice_concentration, Ice_Thickness, and Snow_Thickness.
+#'
+#' @param saved A dataframe containing a summarised month from NEMO-ERSEM model outputs.
+#' @param ice_threshold A value between 0 and 1 defining ice free pixels.
+#' @param ice A TRUE FALSE switch for whether the ice_mod files were included when extracting from NEMO-ERSEM.
+#' @return A dataframe containing a mean monthly time series of all target variables in NEMO-ERSEM outputs.
+#' @family NEMO-MEDUSA averages
+#' @export
+NE_volume_summary <- function(saved, ice_threshold = 0, ice = FALSE) {
+
+  if (ice == TRUE) {
+
+    Groups <- readRDS(file = saved) %>%                                          # Read in wide format data file
+      #    dplyr::filter(!weights < 0) %>%                                            # Drop points on land
+      #    dplyr::mutate(weights = dplyr::na_if(weights, 0)) %>%                      # Replace 0 weights with NA so vector lengths match for weighted mean
+      tidyr::drop_na(Year, Shore) %>%                                            # Drop points outside of the polygons
+      dplyr::group_by(Shore, Year, Month, slab_layer) %>%
+      dplyr::mutate(Ice_pres = ifelse(Ice_conc < ice_threshold, 0, Ice_pres))    # Specify how much ice actually matters when labelling something as ice-affected
+
+    Ice <- dplyr::filter(Groups, Ice_pres > 0) %>%                               # Remove ice free pixels before averaging
+      dplyr::summarise(Ice_Thickness_avg = mean(Ice_Thickness, na.rm = TRUE),    # Get monthly mean sea ice thickness
+                       Snow_Thickness_avg = mean(Snow_Thickness, na.rm = TRUE),         # Get monthly mean snow thickness
+                       Ice_conc_avg = mean(Ice_conc, na.rm = TRUE))                     # Get monthly mean sea ice concentration
+
+    Averaged <- Groups %>%
+      dplyr::summarise(Salinity_avg = stats::weighted.mean(Salinity, weights, na.rm = TRUE), # Get monthly mean salinity
+                       Temperature_avg = stats::weighted.mean(Temperature, weights, na.rm = TRUE),
+                       DIN_avg = stats::weighted.mean(DIN, weights, na.rm = TRUE),
+                       Detritus_avg = stats::weighted.mean(Detritus, weights, na.rm = TRUE),
+                       Phytoplankton_avg = stats::weighted.mean(Phytoplankton, weights, na.rm = TRUE),
+                       Ice_pres = mean(Ice_pres, na.rm = TRUE),                         # Proportion of pixels covered by ice
+                       Meridional_avg = stats::weighted.mean(Meridional, weights, na.rm = TRUE),
+                       Zonal_avg = stats::weighted.mean(Zonal, weights, na.rm = TRUE)) %>%
+      dplyr::left_join(Ice) %>%                                                # Add in ice and snow thicknesses
+      dplyr::ungroup()
+  }
+
+  if (ice == FALSE) {
+
+    Groups <- readRDS(file = saved) %>%                                          # Read in wide format data file
+      tidyr::drop_na(Year, Shore) %>%                                            # Drop points outside of the polygons
+      dplyr::group_by(Shore, Year, Month, slab_layer)
+
+    Averaged <- Groups %>%
+      dplyr::summarise(Salinity_avg = stats::weighted.mean(Salinity, weights, na.rm = TRUE), # Get monthly mean salinity
+                       Temperature_avg = stats::weighted.mean(Temperature, weights, na.rm = TRUE),
+                       NO3_avg = stats::weighted.mean(NO3, weights, na.rm = TRUE),
+                       NH4_avg = stats::weighted.mean(NH4, weights, na.rm = TRUE),
+                       #Detritus_avg = stats::weighted.mean(Detritus, weights, na.rm = TRUE),
+                       Chlorophyll_avg = stats::weighted.mean(Chlorophyll, weights, na.rm = TRUE),
+                       Meridional_avg = stats::weighted.mean(Meridional, weights, na.rm = TRUE),
+                       Zonal_avg = stats::weighted.mean(Zonal, weights, na.rm = TRUE)) %>%
+      dplyr::ungroup()
+  }
+
+
+  return(Averaged) }
+
+#' Summarise NEMO-ERSEM Output into Decadal Grids
+#' This function averages cleaned NEMO-ERSEM monthly summaries into decadal grids.
+#'
+#' The function groups by all spatial variables (Longitude, Latitude, Depth, and Shore zone), and by decade and month.
+#' The mean for every other variable is calculated within these groups.
+#'
+#' @param saved A dataframe containing a summarised month from NEMO-ERSEM model outputs. It must contain the columns:
+#' Longitude, Latitude, Decade, Month, Shore, and Depth.
+#' @param dt Switch for using either data.table or dplyr methods (TRUE/FALSE respectively)
+#' @return A dataframe containing a summarised decade of spatialy resolved NEMO-ERSEM outputs.
+#' @family NEMO-ERSEM averages
+#' @export
+NE_decadal_summary <- function(decade, dt) {
+
+  if(dt == TRUE){                                                               # Run data.table method
+    # data.table::setDT(decade)                                                 # set as a data.table, not needed if decade is already a data.table
+    Averaged <- decade[, lapply(.SD, mean, na.rm = TRUE),                       # Average data columns which aren't groups
+                       by = c("longitude", "latitude", "Decade", "Month", "Shore", "slab_layer")] # Group by pixel and decade
+  } else{                                                                       # Run dplyr method
+    Averaged <- decade %>%
+      dplyr::group_by(longitude, latitude, Decade, Month, Shore, slab_layer) %>%# Group by pixel and decade
+      dplyr::summarise_all(mean, na.rm = TRUE) %>%                              # Average data columns
+      dplyr::ungroup()                                                          # Ungroup
+  }
+  return(Averaged)
 }
